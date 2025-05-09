4f918d41c5c7db62da5d80f2651bcf53903bbf9d
carolinecbahia <carolinecbahia@gmail.com>
2025-05-09 20:35:57 -0300

    C00

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccavalca <ccavalca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 11:29:31 by ccavalca          #+#    #+#             */
+/*   Updated: 2025/04/20 13:43:37 by ccavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
+{
+	char	letra;
+
+	letra = 'a';
+	while (letra <= 'z')
+	{
+		write(1, &letra, 1);
+		letra++;
+	}
+}
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccavalca <ccavalca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 13:42:51 by ccavalca          #+#    #+#             */
+/*   Updated: 2025/04/20 13:53:49 by ccavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letra;
+
+	letra = 'z';
+	while (letra >= 'a')
+	{
+		write(1, &letra, 1);
+		letra--;
+	}
+}
